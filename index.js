ec0ca8c6b45f0b5dae8307ccd777b7b8e379c484
Utkrishth <utkrishth2309@yahoo.com>
2023-03-05 23:57:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,95 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+const fs=require('fs');
+const { type } = require('os');
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+app.use(express.static('public'));
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'/views'));
+
+var signed=false;
+const isSigned=(req,res,next)=>{
+    if(signed){
+        next()
+    }
+    else{
+        res.redirect('/');
+    }
+}
+const isNotSigned=(req,res,next)=>{
+    if(!signed){
+        next()
+    }
+    else{
+        res.redirect('/redirected');
+    }
+}
+
+var data=fs.readFileSync('user_info.json');
+data=JSON.parse(data) 
+var data_keys=Object.keys(data)
+
+app.get('/',isNotSigned,(req,res)=>{
+    res.render('home');
+});
+app.get('/signup',isNotSigned,(req,res)=>{
+    res.render('signup');
+});
+app.get('/redirected',isSigned,(req,res)=>{
+    
+    res.render('redirected_page');
+    
+    
+});
+app.get('/signout',isSigned,(req,res)=>{
+    signed=false
+    res.redirect('/')
+});
+
+app.get('/*',(req,res)=>{
+    res.redirect('/');
+});
+
+app.post('/',isNotSigned,(req,res)=>{
+    if(data_keys.includes(req.body.username)){
+        
+        if(data[req.body.username]==req.body.password){
+            signed=true;
+            
+            res.redirect('/redirected');
+        }
+        else{
+            
+            res.redirect('/')
+        }
+    }
+    else{
+        
+        res.redirect('/')
+    }
+    
+});
+app.post('/signup',isNotSigned,(req,res)=>{
+    var input=req.body;
+    if(data_keys.includes(req.body.username)){
+        res.redirect('/signup');
+    }
+    else{
+        
+        var new_password=String(req.body.password)
+        var new_username=String(req.body.username);
+        
+        
+        data[new_username]=new_password;
+        data=JSON.stringify(data,null,2);
+        fs.writeFile('user_info.json',data,()=>{console.log('Added new user\n',data)});
+        data=JSON.parse(data) 
+        data_keys=Object.keys(data)
+        res.redirect('/')
+    }
+});
+app.listen(3000,()=>{
+    console.log("Listening to port 3000")
+});
